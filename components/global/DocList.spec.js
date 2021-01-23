f06065adb5b69a54f74e121af500ad791a584e4a
Tomoki Morita <tmsongbooks215@gmail.com>
2021-01-23 22:11:57 +0900

    Fix test for DocList component

--- components/global/DocList.spec.js
+++ components/global/DocList.spec.js
@@ -1,17 +1,46 @@
 import { shallowMount, RouterLinkStub } from '@vue/test-utils'
 import DocList from './DocList'
+import ContentLoading from '~/components/ContentLoading'
 
-const factory = ({ props, data }) => {
+const factory = ({ props, data, fetchState }) => {
   return shallowMount(DocList, {
     propsData: props,
     data,
     stubs: {
-      'nuxt-link': RouterLinkStub
+      'nuxt-link': RouterLinkStub,
+      ContentLoading
+    },
+    mocks: {
+      $fetchState: fetchState
     }
   })
 }
 
-test('elements', () => {
+test('fetchState.pending: true', () => {
+  const wrapper = factory({
+    props: {
+      theme: 'foo'
+    },
+    fetchState: {
+      pending: true
+    }
+  })
+  expect(wrapper.text()).toBe('ドキュメントのリストを読み込んでいます...')
+})
+
+test('fetchState.error: true', () => {
+  const wrapper = factory({
+    props: {
+      theme: 'foo'
+    },
+    fetchState: {
+      error: true
+    }
+  })
+  expect(wrapper.text()).toBe('ドキュメントのリストの読み込みに失敗しました。')
+})
+
+test('fetchState.pending: false', () => {
   const docs = [
     { title: 'Title1', slug: 'title1', path: '/docs/foo/title1' },
     { title: 'Title2', slug: 'title2', path: '/docs/foo/title2' },
@@ -25,6 +54,9 @@ test('elements', () => {
       return {
         docs
       }
+    },
+    fetchState: {
+      pending: false
     }
   })
   wrapper.setData({ docs })
