715ec60937c6dacfd4afd21fd6bccc98125b5770
Tomoki Morita <tmsongbooks215@gmail.com>
2021-04-21 16:59:11 +0900

    Update Tailwind configirations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,45 +1,43 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true
-  },
+  mode: "jit",
   theme: {
     container: {
-      padding: '1rem'
+      padding: "1rem",
     },
     screens: {
-      md: '720px'
+      md: "720px",
     },
     fontFamily: {
-      sans: ['"Source Sans Pro"', '"Yu Gothic"', 'YuGothic', 'sans-serif']
+      sans: ['"Source Sans Pro"', '"Yu Gothic"', "YuGothic", "sans-serif"],
     },
     fontSize: {
-      sm: ['15px', '20px'],
-      base: ['17px', '1.65'],
-      '3xl': '1.65rem',
-      '4xl': '2rem'
+      sm: ["15px", "20px"],
+      base: ["17px", "1.65"],
+      "3xl": "1.65rem",
+      "4xl": "2rem",
     },
     extend: {
       colors: {
         gray: {
-          100: '#f6f6f9',
-          200: '#eeefef',
-          700: '#5e5e5f',
-          800: '#374851'
-        }
-      }
-    }
+          100: "#f6f6f9",
+          200: "#eeefef",
+          700: "#5e5e5f",
+          800: "#374851",
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],
   purge: {
-    enabled: process.env.NODE_ENV === 'production',
+    enabled: process.env.NODE_ENV === "production",
     content: [
-      'components/**/*.vue',
-      'content/**/*.md',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'plugins/**/*.js',
-      'nuxt.config.js'
-    ]
-  }
-}
+      "components/**/*.vue",
+      "content/**/*.md",
+      "layouts/**/*.vue",
+      "pages/**/*.vue",
+      "plugins/**/*.js",
+      "nuxt.config.js",
+    ],
+  },
+};
