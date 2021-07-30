9153d63cc72212a85e27a4f41ce1c71fa200149c
Tomoki Morita <tmsongbooks215@gmail.com>
2021-07-31 00:34:33 +0900

    Add site info component

---
+++ components/TheInfo.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="mb-10 p-3 border-2 border-red-300 rounded font-semibold">
+    このサイトはリニューアルされ現在
+    <link-external href="https://jamband.github.io/blog/">jamband/blog</link-external>
+    で管理されています。2021 年 12 月 31 日をもってこのサイトは閉鎖されます。予めご了承ください。
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="flex flex-col min-h-screen">
     <the-header />
-    <nuxt class="flex-grow max-w-screen-md container mx-auto pt-24 pb-10" />
+    <div class="flex-grow max-w-screen-md container mx-auto pt-24 pb-10">
+      <the-info />
+      <nuxt />
+    </div>
     <the-footer />
   </div>
 </template>
